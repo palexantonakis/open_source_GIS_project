1f3431722b0e4a749d112bd29036a544ae470462
Nikolas Schizas <schizasnik@gmail.com>
2019-03-18 23:25:02 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const request = require('request');
+
+// 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02'
+
+const earthquakes = (start, end, callback) =>{
+
+     const url = `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=${start}&endtime=${end}`
+    //const url = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02'
+
+    request({ url: url, json: true }, (error, response)=>{
+        if (error){
+            callback('Unable to connect to earthquakes services', undefined)
+        } else if (response.body.features === undefined){
+            callback('Unable to find earthquake data', undefined)
+        } else {
+            callback(undefined, response.body.features[0])
+        }
+    })
+}
+
+
+
+
+module.exports = earthquakes
