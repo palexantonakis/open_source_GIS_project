e2b9625e66c1b46fef232e9b05c63a9da53ffbd5
Nikolas Schizas <schizasnik@gmail.com>
2019-03-19 01:01:57 +0200

    pre work for dates

---
+++ readme.md
@@ -0,0 +1,2 @@
+url for date picker
+https://github.com/qodesmith/datepicker#readme
--- server.js
+++ server.js
@@ -2,20 +2,25 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const port = 3000
-const publicDirectoryPath = path.join(__dirname,'./public')
+const publicDirectoryPath = path.join(__dirname, './public')
+const moment = require('moment')
 
 var earthquakes = require('./app.js')
-
-let poutsa;
-
-earthquakes('2017-01-02', '2017-01-03', (error,data)=>{
-    poutsa = data;
-})
 
 app.use(express.static(publicDirectoryPath))
 
 app.get('/', (req, res) => res.sendFile(publicDirectoryPath))
 
-app.get('/json', (req, res) => res.json(poutsa));
+app.get('/json', (req, res) => {
+    const date = req.query.date;
+    const new_date = moment(date);
+    const new_date_plus_one = new_date.clone().add(1, 'days');
+    const start = new_date.format('YYYY-MM-DD');
+    const end = new_date_plus_one.format('YYYY-MM-DD');
+    earthquakes(start,end, (error,data)=>{
+        res.json(data);
+    })
+});
+
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`))
